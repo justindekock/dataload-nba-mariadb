5ad364f46eeb735d5a9896f666bc6bd53fc3fd0f
justindekock <jdekock17@gmail.com>
2025-05-19 22:33:07 -0500

    working player and team data classes in clean.py

---
+++ src/nbaload-mariadb/fetch.py
@@ -0,0 +1,38 @@
+from time import sleep
+from nba_api.stats.endpoints import leaguegamefinder, playerindex, teaminfocommon, playbyplayv3
+
+def game_logs(game_date, game_date_to=None, player_team = 'P', lg = 'NBA'):
+    retries = 0
+    while retries < 3:
+        try:
+            df = leaguegamefinder.LeagueGameFinder(
+                player_or_team_abbreviation=player_team,
+                league_id_nullable= '10' if lg == 'WNBA' else ('20' if lg == 'G' else '00'), 
+                date_from_nullable=game_date,
+                date_to_nullable=game_date_to if game_date_to else game_date
+            ).get_data_frames()[0]
+            
+            # CRUCIAL -- ADD LG TO THIS DF
+            df['lg'] = lg
+            
+            return df
+        except Exception as e:
+            print(e)
+            sleep(15)
+    
+    
+def active_players():
+    retries = 0
+    while retries < 3:
+        try:
+            return playerindex.PlayerIndex(active_nullable=1).get_data_frames()[0]
+        except Exception as e:
+            print(e)
+    
+def teams():
+    retries = 0
+    while retries < 3:
+        try:
+            return teaminfocommon.TeamInfoCommon()
+        except Exception as e:
+            print(e)
---
+++ src/nbaload-mariadb/main.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+from dbamdb import conn
+import fetch
+import clean
+
+#dbc = conn.Conn()
+
+#dbc.show_db()
+
+game_date = (datetime.today() - timedelta(1)).strftime('%m/%d/%Y')
+
+raw_tm = fetch.game_logs(game_date=game_date, player_team='T', lg='NBA')
+raw_pl = fetch.game_logs(game_date=game_date, player_team='P', lg='NBA')
+
+team_data = clean.TeamData(raw_tm)
+
+# print(team_data.tgame_df)
+player_data = clean.PlayerData(raw_pl, team_data.tgame_df)
